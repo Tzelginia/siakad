71b28fed64f81aeb569f0f06ea77236f65fd6e4c
tzelginianc <tzelginia78@gmail.com>
2022-03-30 16:41:30 +0700

    perbarui model

---
+++ app/Models/Mahasiswa.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Models;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\Mahasiswa as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model; //Model Eloquen
+
+class Mahasiswa extends Model
+{
+    protected $table='mahasiswa'; // Eloquent akan membuat model mahasiswa menyimpan record di tabel mahasiswa
+ protected $primaryKey = 'Nim'; // Memanggil isi DB Dengan primarykey
+ /**
+ * The attributes that are mass assignable.
+ *
+ * @var array
+ */
+ protected $fillable = [
+ 'Nim',
+ 'Nama',
+ 'Kelas',
+ 'Jurusan',
+ ];
+}
